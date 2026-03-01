5d1b42c2a53bb42e81bfd4c738cc7e3852c99671
evenordinstorm <evenordinstorm@gmail.com>
2026-03-01 12:45:35 +0000

    style: menus and settings options

--- src/components/CallControlBar/SettingsMenu/SettingsMenu.tsx
+++ src/components/CallControlBar/SettingsMenu/SettingsMenu.tsx
@@ -1,5 +1,23 @@
 import styles from "./SettingsMenu.module.css";
+import { Disc, AudioLines, Camera } from "lucide-react";
 
 export function SettingsMenu() {
-  return <div className={styles.settingsMenu}>Settings menu</div>;
+  return (
+    <div className={styles.settingsMenu}>
+      <ul className={styles.list}>
+        <li>
+          <Disc size={16} />
+          <div>Record</div>
+        </li>
+        <li>
+          <AudioLines size={16} />
+          <div>Audio Settings</div>
+        </li>
+        <li>
+          <Camera size={16} />
+          <div>Video Effects</div>
+        </li>
+      </ul>
+    </div>
+  );
 }
